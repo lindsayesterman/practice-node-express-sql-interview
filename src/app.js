64a2aa8030a993f85f89777897519d5c175eb15d
Lindsay Esterman <lindsayesterman@gmail.com>
2020-12-13 00:15:55 -0500

    practice node/express/postgresql interview

---
+++ src/app.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const errorHandler = require('./middleware/error-handler')
+const TodoService = require('./todo/todo-service')
+const xss = require('xss')
+const jsonParser = express.json()
+const path = require('path')
+const todoRouter = require('./todo/todo-router')
+const app = express()
+
+const morganOption = (NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+app.use(morgan(morganOption, {
+  skip: () => NODE_ENV === 'test',
+}))
+app.use(cors())
+app.use(helmet())
+app.use(todoRouter);
+app.use(express.static('public'))
+
+
+app.use(errorHandler)
+
+module.exports = app
